13a7dd37784929190793472ea21bef15e8026184
Christophe DELIQUAIRE <christophe.deliquaire@gmail.com>
2022-01-16 18:54:50 +0100

    sveltekit frontend

---
+++ apps/frontend/.svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,6 @@
+import * as module from '../entries/pages/error.svelte.js';
+
+export { module };
+export const entry = 'error.svelte-9116ca50.js';
+export const js = ["error.svelte-9116ca50.js","chunks/vendor-91d27c94.js"];
+export const css = [];
